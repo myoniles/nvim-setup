1a7bab9e3462ee497adbcf6c8e4ec837b93012f0
Michael Yoniles <BLMdnst8@gmail.com>
2023-10-15 21:27:47 -0500

    add some cool remaps

--- nvim/lua/mikeyon/remap.lua
+++ nvim/lua/mikeyon/remap.lua
@@ -22,4 +22,25 @@ vim.api.nvim_create_autocmd("TermOpen", {pattern = '*', callback = function()
 end
 })
 
+-- Move with blocks in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep J at linestart
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- leader p to paste without writing copy buffer
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- No EX mode
+vim.keymap.set("n", "Q", "<nop>")
+
+-- format
+vim.keymap.set("n", "<leader>F", vim.lsp.buf.format)
+
 -- TODO toggle spelling
