720c7a9334f9da13ec84543581ddae55fca5547f
Michael Yoniles <BLMdnst8@gmail.com>
2024-10-15 22:56:07 -0500

    Nerdtree

--- nvim/lua/mikeyon/remap.lua
+++ nvim/lua/mikeyon/remap.lua
@@ -3,7 +3,7 @@ local silent = { silent = true, noremap = true }
 map("", "<Space>", "<Nop>", silent)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.keymap.set("n", "<leader>N", vim.cmd.Ex)
+--vim.keymap.set("n", "<leader>N", vim.cmd.Ex)
 
 -- I like these for easy buffer and tab movement
 vim.keymap.set("n", "]b", vim.cmd.bn)
